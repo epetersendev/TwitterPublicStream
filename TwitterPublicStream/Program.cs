5427bb55521e56c4bc0b27d1034d2340d1507558
epetersendev <37513663+epetersendev@users.noreply.github.com>
2020-07-30 07:03:23 -0500

    initial load

---
+++ TwitterPublicStream/Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using TwitterPublicStream.Repositories;
+using TwitterPublicStream.Services;
+
+namespace TwitterPublicStream
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var services = ConfigureServices();
+            var serviceProvider = services.BuildServiceProvider();
+            serviceProvider.GetService<ConsoleApplication>().Run();
+        }
+
+        private static IServiceCollection ConfigureServices()
+        {
+            IServiceCollection services = new ServiceCollection();
+            services.AddSingleton<IUrlsRepository, UrlsRepository>();
+            services.AddSingleton<IHashTagRepository, HashTagRepository>();
+            services.AddSingleton<IEmojiRepository, EmojiRepository>();
+            services.AddSingleton<ITweetRepository, TweetRepository>();
+            services.AddSingleton<IMediaService, MediaService>();
+            services.AddSingleton<IUrlsService, UrlsService>();
+            services.AddSingleton<IHashTagService, HashTagService>();
+            services.AddSingleton<IEmojiService, EmojiService>();
+            services.AddSingleton<ITweetService, TweetService>();
+            services.AddSingleton<ITwitterStreamService, TwitterStreamService>();
+            services.AddSingleton<ConsoleApplication>();
+            return services;
+        }
+
+    }
+}
